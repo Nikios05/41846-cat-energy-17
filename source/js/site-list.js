ef99fe3c39b6847e29f3e95c241a0999c1fe489a
Nikios <kochetkov.nick93@yandex.ru>
2019-08-07 22:21:05 +0300

    Добавленые декоративые адаптивные элементы

--- source/js/site-list.js
+++ source/js/site-list.js
@@ -5,6 +5,7 @@ export function siteList() {
 
   btnOpen.addEventListener("click", () => {
     console.log("ok");
+    btnOpen.classList.toggle("main-nav__toggle--close");
     menu.classList.toggle("site-list--on");
     item.forEach((i) => {
       i.classList.toggle("site-list__item--on");
