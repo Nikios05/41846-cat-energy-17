847a0c93736b16a992b334b8f5be57d7a88ee18c
Nikios <kochetkov.nick93@yandex.ru>
2019-08-10 14:00:00 +0300

    Добавлена поддержка меню навигации без js

--- source/js/app.js
+++ source/js/app.js
@@ -132,45 +132,4 @@ if (browser >= tableWidth && browser < desktopWidth) {
     };
   };
 }
-"use strict";
-
-var catName = document.querySelector("[name='cat-name'");
-var catWeight = document.querySelector("[name='cat-weight'");
-var catAge = document.querySelector("[name='cat-age'");
-var email = document.querySelector("[type='email'");
-var phoneNumber = document.querySelector("[type='tel'");
-var submit = document.querySelector(".form__submit");
-submit.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("ok");
-  var errorMessage = "";
-
-  if (catWeight.value == "") {
-    catWeight.classList.add("form__text--invalid");
-    errorMessage = errorMessage + "Поле имени обязательно для заполнения\n";
-  } else if (catWeight.value > 25) {
-    catWeight.classList.add("form__text--invalid");
-    errorMessage = errorMessage + "Если ваш кот столько весит, значит вы побили мировой рекорд\n";
-  }
-
-  if (catName.value == "") {
-    catName.classList.add("form__text--invalid");
-    errorMessage = errorMessage + "Поле веса обязательно для заполнения\n";
-  } else if (catName.value.length > 30) {
-    catName.classList.add("form__text--invalid");
-    errorMessage = errorMessage + "У вашего кота слишком длинное имя, может быть есть домашння кличка?\n";
-  }
-
-  if (email.value == "") {
-    email.classList.add("form__text--invalid");
-    errorMessage = errorMessage + "Поле email обязательно для заполнения\n";
-  }
-
-  if (phoneNumber.value == "") {
-    phoneNumber.classList.add("form__text--invalid");
-    errorMessage = errorMessage + "Поле номера телефона обязательно для заполнения\n";
-  }
-
-  alert(errorMessage);
-});
 //# sourceMappingURL=app.js.map
